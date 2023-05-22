489380db481aea18851f271f84ca53f5d561bc3f
seyma <symbglr54@gmail.com>
2023-05-22 18:32:09 +0300

    change

--- java.js
+++ java.js
@@ -8,9 +8,9 @@ button.addEventListener("click", result)
 function result(){
     // const index = Math.round(Math.random()*10)
     audio.play();
-    const index = Math.round(Math.random()*color.length)
-    body.style.backgroundColor =color[index];
-    button.style.color=color[index-1]
-    button.style.backgroundColor=color[index-2]
+    // const index = Math.round(Math.random()*color.length)
+    // body.style.backgroundColor =color[index];
+    // button.style.color=color[index-1]
+    // button.style.backgroundColor=color[index-2]
 
 }
